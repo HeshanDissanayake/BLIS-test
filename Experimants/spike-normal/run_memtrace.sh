cc5cf2d34f7f59dc250dd7cccc3b7ea6766cda06
HeshanDissanayake <tharindudissa18@gmail.com>
2026-03-03 06:44:33 +1100

    .

--- Experimants/spike-normal/run_memtrace.sh
+++ Experimants/spike-normal/run_memtrace.sh
@@ -10,10 +10,16 @@ fi
 cd "${exp_dir}" || exit 1
 
 
-SPIKE_DIR=/home/heshds/working_dir/cva6-sdk/riscv-isa-sim/build/spike
-EXP_TOOLS=/home/heshds/working_dir/regsw_tests/BLIS-test/Experimants/Experiment_tools
-IMG_DIR=/home/heshds/working_dir/regsw_tests/BLIS-test/Experimants/spike-normal/images
-UTIL=/home/heshds/working_dir/regsw_tests/BLIS-test/Experimants/spike-normal/util
+SPIKE_DIR=spike
+EXP_TOOLS=/home/heshds/working_dir/BLIS-test/Experimants/Experiment_tools
+IMG_DIR=/home/heshds/working_dir/BLIS-test/Experimants/spike-normal/images
+UTIL=/home/heshds/working_dir/BLIS-test/Experimants/spike-normal/util
+
+# SPIKE_DIR=/home/heshds/working_dir/cva6-sdk/riscv-isa-sim/build/spike
+# EXP_TOOLS=/home/heshds/working_dir/regsw_tests/BLIS-test/Experimants/Experiment_tools
+# IMG_DIR=/home/heshds/working_dir/regsw_tests/BLIS-test/Experimants/spike-normal/images
+# UTIL=/home/heshds/working_dir/regsw_tests/BLIS-test/Experimants/spike-normal/util
+
 
 cat config.json \
 | python3 ${EXP_TOOLS}/expand_config.py -i MC,NC,KC,MR,N_start,N_end,N_step \
