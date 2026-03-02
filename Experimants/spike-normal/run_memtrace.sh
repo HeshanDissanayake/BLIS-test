b8c11f4ce483aa4bcdee39b097a8d930540045f3
HeshanDissanayake <tharindudissa18@gmail.com>
2026-03-03 06:58:57 +1100

    run_memtraces updated

--- Experimants/spike-normal/run_memtrace.sh
+++ Experimants/spike-normal/run_memtrace.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 exp_dir=$1
 
+ROOT=$(pwd)
+SPIKE_DIR=spike
+EXP_TOOLS=${ROOT}/../Experiment_tools
+IMG_DIR=${ROOT}/images
+UTIL=${ROOT}/util
+
 # Safety check
 if [[ ! -d "$exp_dir" ]]; then
     echo "Error: '$exp_dir' is not a directory" >&2
@@ -9,24 +15,12 @@ fi
 
 cd "${exp_dir}" || exit 1
 
-
-SPIKE_DIR=spike
-EXP_TOOLS=/home/heshds/working_dir/BLIS-test/Experimants/Experiment_tools
-IMG_DIR=/home/heshds/working_dir/BLIS-test/Experimants/spike-normal/images
-UTIL=/home/heshds/working_dir/BLIS-test/Experimants/spike-normal/util
-
-# SPIKE_DIR=/home/heshds/working_dir/cva6-sdk/riscv-isa-sim/build/spike
-# EXP_TOOLS=/home/heshds/working_dir/regsw_tests/BLIS-test/Experimants/Experiment_tools
-# IMG_DIR=/home/heshds/working_dir/regsw_tests/BLIS-test/Experimants/spike-normal/images
-# UTIL=/home/heshds/working_dir/regsw_tests/BLIS-test/Experimants/spike-normal/util
-
-
 cat config.json \
 | python3 ${EXP_TOOLS}/expand_config.py -i MC,NC,KC,MR,N_start,N_end,N_step \
 | python3 ${EXP_TOOLS}/run_per_config.py  ${UTIL}/spike.expect  MC NC KC MR N_start N_end N_step -a "${SPIKE_DIR}" -a "${IMG_DIR}" 
 
      
-
+python3 ${ROOT}/util/recursive_split_parallel.py ${ROOT}/${exp_dir}/memtraces "LOG MARKER"
 
 
 
